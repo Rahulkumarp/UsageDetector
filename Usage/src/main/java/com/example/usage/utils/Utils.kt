92c3901f732a1112cb681a39ca57765655bcf510
DS-LAP-0182\User <rahulk10@damcogroup.com>
2022-11-16 13:28:37 +0530

    Applied code for android 10 and above code for file structure

--- Usage/src/main/java/com/example/usage/utils/Utils.kt
+++ Usage/src/main/java/com/example/usage/utils/Utils.kt
@@ -2,6 +2,7 @@ package com.example.usage.utils
 
 import android.content.ContentValues
 import android.content.Context
+import android.os.Build
 import android.os.Environment
 import android.provider.MediaStore
 import java.io.File
@@ -19,35 +20,48 @@ object Utils {
         return formattedDate
     }
 
-     fun setDataInContentProvider(context: Context,fileName: String, mime_type: String, folder : String) : OutputStream? {
-         var imageOutStream: OutputStream? = null
-         if (!File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS+"/$folder") ,
-                 fileName
-             ).exists()
-         ) {
+    fun setDataInContentProvider(context: Context,fileName: String, mime_type: String, folder : String) : OutputStream? {
+        var imageOutStream: OutputStream? = null
+        if (!File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS+"/$folder") ,
+                fileName
+            ).exists()
+        ) {
+
+            if(Build.VERSION.SDK_INT > Build.VERSION_CODES.Q){
+                val values = ContentValues().apply {
+                    put(MediaStore.Images.Media.DISPLAY_NAME, fileName)
+                    put(MediaStore.Images.Media.MIME_TYPE, "csv")
+                    put(MediaStore.Images.Media.RELATIVE_PATH, Environment.DIRECTORY_DOCUMENTS+"/$folder")
+                }
 
 
-             val values = ContentValues().apply {
-                 put(MediaStore.Images.Media.DISPLAY_NAME, fileName)
-                 put(MediaStore.Images.Media.MIME_TYPE, "csv")
-                 put(MediaStore.Images.Media.RELATIVE_PATH, Environment.DIRECTORY_DOCUMENTS+"/$folder")
-             }
+                context.contentResolver.run {
+                    val uri = context.contentResolver.insert(
+                        MediaStore.Files.getContentUri("external"), values
+                    )
+                    imageOutStream = openOutputStream(uri!!)!!
 
+                }
 
-             context.contentResolver.run {
-                 val uri = context.contentResolver.insert(
-                     MediaStore.Files.getContentUri("external"), values
-                 )
-                 imageOutStream = openOutputStream(uri!!)!!
+                return imageOutStream!!
+            }else{
 
-             }
+                val path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).absolutePath
+                val newPath = "$path/$folder"
+                val file = File(newPath)
+                file.mkdir()
+                val fileCreate = File(file,fileName)
+                fileCreate.createNewFile()
+                imageOutStream =   FileOutputStream(fileCreate,true)
+                return imageOutStream
 
-             return imageOutStream!!
-         }else {
+            }
 
-             return null
-         }
-         }
+        }else {
+
+            return null
+        }
+    }
 
 
 }
